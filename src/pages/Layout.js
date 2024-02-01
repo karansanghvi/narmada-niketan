32da0e93126ddbcc8b9f93da3c94415612e87572
Karan Sanghvi <karansanghvi1303@gmail.com>
2024-02-02 00:51:42 +0530

    hero banner done

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Outlet, Link } from "react-router-dom";
 import logo from '../assets/images/narmada_niketan_logo.jpeg';
+import '../App.css';
 
 function Layout() {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -14,14 +15,14 @@ function Layout() {
       <nav className="bg-white border-gray-200 shadow-lg">
         <div className="flex flex-wrap items-center justify-between mx-auto p-4 ml-4 mr-4">
           <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src={logo} className="h-20" alt="NFC Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap text-black">Narmadaben Charitable Foundation</span>
+            <img src={logo} className="md:h-20 h-10" alt="NFC Logo" />
+            <span className="self-center md:text-2xl font-semibold whitespace-nowrap text-black text-sm">Narmadaben Charitable Foundation</span>
           </a>
           
           {/* hamburger */}
           <button
             onClick={toggleMenu}
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-black rounded-lg hover:bg-backgroundColor focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-black dark:hover:bg-backgroundColor dark:focus:ring-black md:hidden"
             aria-controls="navbar-cta"
             aria-expanded={isMenuOpen ? "true" : "false"}
           >
@@ -35,28 +36,28 @@ function Layout() {
           <div className={`items-center justify-between w-full md:hidden ${isMenuOpen ? 'block' : 'hidden'}`} id="navbar-cta">
             <ul className="flex flex-col font-medium p-4 mt-4 border  rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
               <li>
-                <Link to="/" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 text-black md:dark:hover:bg-transparent"aria-current="page">Home</Link>
+                <Link to="/" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent  text-black md:dark:hover:bg-transparent font-semibold"aria-current="page">Home</Link>
               </li>
               <li>
-                <Link to="/aboutus" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 text-black md:dark:hover:bg-transparent"aria-current="page">About Us</Link>
+                <Link to="/aboutus" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent  text-black md:dark:hover:bg-transparent font-semibold"aria-current="page">About Us</Link>
               </li>
               <li>
-                <Link to="/ourfacilities" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 text-black md:dark:hover:bg-transparent"aria-current="page">Our Facilities</Link>
+                <Link to="/ourfacilities" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent  text-black md:dark:hover:bg-transparent font-semibold"aria-current="page">Our Facilities</Link>
               </li>
               <li>
-                <a href="/codeofconduct" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 text-black md:dark:hover:bg-transparent">Code Of Conduct</a>
+                <a href="/codeofconduct" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent  text-black md:dark:hover:bg-transparent font-semibold">Code Of Conduct</a>
               </li>
               <li>
-                <a href="/celebrations" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 text-black md:dark:hover:bg-transparent">Celebrations</a>
+                <a href="/celebrations" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent  text-black md:dark:hover:bg-transparent font-semibold">Celebrations</a>
               </li>
               <li>
-                <a href="/tariff" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 text-black md:dark:hover:bg-transparent">Tariff</a>
+                <a href="/tariff" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent  text-black md:dark:hover:bg-transparent font-semibold">Tariff</a>
               </li>
               <li>
-                <a href="/testimonials" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 text-black md:dark:hover:bg-transparent">Testimonials</a>
+                <a href="/testimonials" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent  text-black md:dark:hover:bg-transparent font-semibold">Testimonials</a>
               </li>
               <li>
-                <Link to="/donate" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Donate</Link>
+                <Link to="/donate" className="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-3 py-2 text-center bg-backgroundColor">Donate</Link>
               </li>
             </ul>
           </div>
@@ -65,28 +66,28 @@ function Layout() {
           <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
             <ul className="flex flex-col md:flex-row font-medium p-4 md:p-0 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:border-0 md:bg-white">
               <li>
-                <Link to="/" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 text-black md:dark:hover:bg-transparent"aria-current="page">Home</Link>
+                <Link to="/" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent  text-black md:dark:hover:bg-transparent font-semibold"aria-current="page">Home</Link>
               </li>
               <li>
-                <Link to="/aboutus" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 text-black md:dark:hover:bg-transparent">About Us</Link>
+                <Link to="/aboutus" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent  text-black md:dark:hover:bg-transparent font-semibold">About Us</Link>
               </li>
               <li>
-                <Link to="/ourfacilities" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 text-black md:dark:hover:bg-transparent">Our Facilities</Link>
+                <Link to="/ourfacilities" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent  text-black md:dark:hover:bg-transparent font-semibold">Our Facilities</Link>
               </li>
               <li>
-                <a href="/codeofconduct" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 text-black md:dark:hover:bg-transparent">Code Of Conduct</a>
+                <a href="/codeofconduct" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent  text-black md:dark:hover:bg-transparent font-semibold">Code Of Conduct</a>
               </li>
               <li>
-                <a href="/celebrations" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 text-black md:dark:hover:bg-transparent">Celebrations</a>
+                <a href="/celebrations" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent  text-black md:dark:hover:bg-transparent font-semibold">Celebrations</a>
               </li>
               <li>
-                <a href="/tariff" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 text-black md:dark:hover:bg-transparent">Tariff</a>
+                <a href="/tariff" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent  text-black md:dark:hover:bg-transparent font-semibold">Tariff</a>
               </li>
               <li>
-                <a href="/testimonials" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 text-black md:dark:hover:bg-transparent">Testimonials</a>
+                <a href="/testimonials" className="block py-2 px-3 md:p-0  rounded  md:hover:bg-transparent  text-black md:dark:hover:bg-transparent font-semibold">Testimonials</a>
               </li>
               <li>
-                <Link to="/donate" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Donate</Link>
+                <Link to="/donate" className="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-3 py-2 text-center bg-backgroundColor">Donate</Link>
               </li>
             </ul>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '62': '62px',
+        '60': '60px',
+      },
+      textColor: {
+        textColor: '#39DB4A',
+      },
+      backgroundColor: {
+        backgroundColor: '#39DB4A',
+      },
+      borderColor: {
+        borderColor: '#39DB4A',
+      },
+    },
   },
   variants: {
     extend: {},
