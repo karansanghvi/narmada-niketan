78912af36d0b7e3952cad914e1c7d63198855245
Karan Sanghvi <karansanghvi1303@gmail.com>
2024-02-02 11:08:41 +0530

    added home about

--- src/components/HomeAbout.js
+++ src/components/HomeAbout.js
@@ -3,7 +3,7 @@ import React from 'react'
 function HomeAbout() {
   return (
     <div>
-      <h1 className='text-black'>this is the home about section</h1>
+      <h1 className='text-blacke'>this is the home about section</h1>
     </div>
   )
 }
