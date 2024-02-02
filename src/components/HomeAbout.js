21fc09f940f3f769e108ce532bd412900ae8e78a
Karan Sanghvi <karansanghvi1303@gmail.com>
2024-02-02 11:08:16 +0530

    added home about file

---
+++ src/components/HomeAbout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomeAbout() {
+  return (
+    <div>
+      <h1 className='text-black'>this is the home about section</h1>
+    </div>
+  )
+}
+
+export default HomeAbout
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import HomeAbout from '../components/HomeAbout';
 
 function Home() {
   return (
     <>
       <Hero/>
+      <br /> <br /> <br /> <br /> <br />
+      <HomeAbout/>
     </>
   )
 }
