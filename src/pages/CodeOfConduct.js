1a8d29c4fcc26e80020e86d95cd7b4b6a7cf3166
Karan Sanghvi <karansanghvi1303@gmail.com>
2024-02-05 10:47:48 +0530

    code of conduct page done

--- src/pages/CodeOfConduct.js
+++ src/pages/CodeOfConduct.js
@@ -1,10 +1,115 @@
 import React from 'react'
+import Footer from '../components/Footer'
 
 function CodeOfConduct() {
   return (
-    <div>
-      Code Of Conduct
-    </div>
+    <>
+      <div className='flex justify-center mt-36'>
+        <h1 className="font-semibold text-5xl md:text-6xl lg:text-7xl text-center text-black">
+          Code Of Conduct For Residents
+        </h1>
+      </div>
+      <div className='ml-2 md:ml-56 mr-2 md:mr-72 mt-4'>
+        <b className='text-black text-xl'>Criteria:</b>
+        <ol>
+          <li className='text-black'>1) Male applicants should be of more than 60 years of age & female applicants should be more than 55 years of age.</li>
+          <li className='text-black'>2) Should not have contracted any contagious diseases.</li>
+          <li className='text-black'>3) Should be mobile and able to take care of themselves.</li>
+        </ol>
+
+        <div className='mt-4'>
+          <b className='text-black text-xl'>Admission Time:</b>
+          <p className='text-black'>Every evening 4:00 p.m. to 5:30 p.m.</p>
+        </div>
+
+        <div className='mt-4'>
+          <b className='text-black text-xl'>Visitor's Hours:</b>
+          <p className='text-black'>Every morning 10:00 a.m. to 11:15 a.m. and every afternoon 1:00 p.m. and 5:45 p.m.</p>
+        </div>
+
+        <div className='mt-4'>
+          <b className='text-black text-xl'>Other Information:</b>
+          <p className='text-black'>Admission form (Rs. 10/-) needs to be filled up for admission. If he/she desires, the residents are free to be a member of the resident's association through which the extra held by the residents could be kept in the custody of the trust thereby ensuring safety.</p>
+        </div>
+
+        <div className='mt-4'>
+          <b className='text-black text-xl'>Conduct Code For Residents Once Admitted:</b>
+          <ol type="1" className='text-black ml-3' style={{ listStyleType: 'decimal' }} >
+            <li className='text-black'>
+              Admission is provided initially for 3 months irrespective of the duration requested. Later it is extended on request
+            </li>
+            <li className='text-black'>
+              Each applicant goes through a physical examination by the Trust's doctor. On satisfactory reports based on criteria mentioned above, admission is given.
+            </li>
+            <li className='text-black'>
+              Please see the "Tariff" link for the monthly charges and deposit
+            </li>
+            <li className='text-black'>
+              Applicant and his/her relatives would have to sign on a bond paper at the time of admission
+            </li>
+            <li className='text-black'>
+              Residents can take short leaves from their stay in the Homes. However, they would not be refunded for their absence from the Homes.
+            </li>
+            <li className='text-black text-justify'>
+              All residents would have to follow the Conduct Code and comply with the regulations of the Manager(s) / Administrator(s). On non-compliance of the Conduct Code, Manager(s) / Administrator(s) have the right to ask the resident to leave the Homes' premises. If the Trustees find that any resident's behaviour is not in tune with the Conduct Code and the objects of the Trust, they may give a written notice to the resident to vacate their rooms and leave the Homes' premises within a week.
+            </li>
+            <li className='text-black'>
+              Residents need to carry the items of their personal consumption (for e.g. toileteries like toothbrush, toothpaste, hairoil, washing soap & detergent, bathing soap, clothes, other hobby items like books, cassette players, CD players, etc.)
+            </li>
+            <li className='text-black'>
+              Consumption of alcohol or any intoxicants like smoking or any form of tobacco within the Homes premises is not permitted.
+            </li>
+            <li className='text-black text-justify'>
+              Residents are advised not to keep lots of cash or expensive items or jewelry with them in the rooms. Generally, the Homes have security personnel of their own and jaywalkers are not allowed. However, in case of any mishap, the unfortunate loss of any expensive item/s belonging to the residents would not be the responsibility of the Homes and the Trust. 
+            </li>
+            <li className='text-black '>
+              Residents should not cook in their rooms.
+            </li>
+            <li className='text-black text-justify'>
+              Unless they have opted for room service (available at a nominal costs), residents are advised to consume the meals and snacks inside the dining hall/s and not carry it to their rooms
+            </li>
+            <li className='text-black'>
+              It is the responsibility of the residents to keep their rooms and surroundings clean. Please do not engage in activities that would spoil the cleanliness at the Homes. 
+            </li>
+            <li className='text-black text-justify'>
+              Residents are advised to direct their complaints to the Manager(s) / Administrator(s) and not the other employees or contract workers. Residents may even articulate their complaints to the Trustee(s) in written form. These could be provided to them in person whenever they visit the Homes or mailed to the Registered Office address. 
+            </li>
+            <li className='text-black'>
+              Volume of radio, transistor, tape recorder, CD player or any other instrument should be kept at a reasonable level so as to not disturb others around you
+            </li>
+            <li className='text-black'>
+              Residents may step out of the Homes' premises for a short duration to take a walk or visit nearby places only after informing the Manager(s) / Administrator(s). At the time of leaving their rooms, residents should lock their rooms after switching off all the lights and ensuring that the taps are shut tight.
+            </li>
+            <li className='text-black'>
+              In case of residents being in ill-health or physically unwell, they should inform the Manager(s) / Administrator(s) immediately who would in turn ensure that the residents' relatives are made aware of the same through phone or mail as applicable.
+            </li>
+            <li className='text-black'>
+              In case of the unfortunate event of serious illness or eventual death of any resident, the Manager(s) / Administrator(s) would inform the same to the relatives of the resident(s) as mentioned in the bond paper signed by the resident(s)/relative(s) at the time of application. In case the relatives do not reach the Homes within 8 hours of the unfortunate event of death, the Trust would initiate and proceed with the cremation rituals.
+            </li>
+            <li className='text-black'>
+              Residents are free to bring their own gadgets and white goods (like TV, refrigerators,etc) to their rooms. They would obviously need to pay for the service charges (for e.g. cable charges) to the respective service providers. They might also have to pay some nominal charges to the Homes/Trust towards the same (for e.g. nominal charges are levied for refrigerator by the Homes). 
+            </li>
+            <li className='text-black'>
+              Residents should wash their own clothes, keep their rooms clean, fill their own water for drinking as well as hot water for bathing, clean the utensils as well as keep their toilet/laboratory clean. These services are also provided at a nominal cost in case requested
+            </li>
+            <li className='text-black'>
+              The Trust / Homes have the full right to change the Conduct Code and rules from time to time
+            </li>
+          </ol>
+        </div>
+
+        <div className='mt-4'>
+          <b className='text-black text-xl'>Facilities and Rules For Any Camp:</b>
+          <p className='text-black text-justify'>
+            Here is a facility for accommodating up to 25 people for residential camps like meditation camps, reiki, children activities, summer camps, etc. Organizations interested for the same would be provided with a spacious hall with microphone/public address system, food - beverages - snacks facilities for the campers, chairs - tables for stage and audience. The camp could be for a period of 1 to 7 days. For organizing such a camp, contact at Narmada Niketan or Vishram Dham.
+          </p>
+        </div>
+      </div>
+
+      <br />
+
+      <Footer/>
+    </>
   )
 }
 
